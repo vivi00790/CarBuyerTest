9ff73e743f48efcd92bb3afc1c1351737612b357
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-31 01:02:52 +0800

    pass test Equal_start_price_and_car_price_should_return_0_0

--- CarBuyerTest/CarBuyerTest.cs
+++ CarBuyerTest/CarBuyerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarBuyerTest
@@ -7,15 +8,22 @@ namespace CarBuyerTest
     public class CarBuyerTest
     {
         [TestMethod]
-        public void equal_start_prize_and_car_prize_should_return_0_0()
+        public void Equal_start_price_and_car_price_should_return_0_0()
         {
+            Assert.IsTrue(AllElementsAreEqual(setExceptArray(0, 0), PriceCounter.Count(8000, 8000, 1000, 1.5)));
+        }
 
-            Assert.AreEqual(setExceptArray(0, 0), PriceCounter.Count(8000, 8000, 1000, 1.5));
+
+        private bool AllElementsAreEqual(int[] expect, int[] actual)
+        {
+            return expect.Where((_, i) => _ == actual[i]).Count() == actual.Length;
         }
 
         private int[] setExceptArray(int expectMonth, int expectPrize)
         {
             return new[] {expectMonth, expectPrize};
         }
+
+        
     }
 }
---
+++ CarBuyerTest/PriceCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBuyerTest
+{
+    class PriceCounter
+    {
+        public static int[] Count(int startPrice, int targetStartPrice, int savingPerMonth, double decreasingRate)
+        {
+            return new[] {(targetStartPrice - startPrice)/savingPerMonth,targetStartPrice-startPrice};
+        }
+    }
+}
