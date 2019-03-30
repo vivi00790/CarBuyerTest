db2fc0ae2d87dbca1a75f59eaf87e4f356fd225a
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-31 00:44:01 +0800

    setup project, add a test

---
+++ CarBuyerTest/CarBuyerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarBuyerTest
+{
+    [TestClass]
+    public class CarBuyerTest
+    {
+        [TestMethod]
+        public void equal_start_prize_and_car_prize_should_return_0_0()
+        {
+
+            Assert.AreEqual(setExceptArray(0, 0), PriceCounter.Count(8000, 8000, 1000, 1.5));
+        }
+
+        private int[] setExceptArray(int expectMonth, int expectPrize)
+        {
+            return new[] {expectMonth, expectPrize};
+        }
+    }
+}
