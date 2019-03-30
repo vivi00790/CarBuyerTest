a154e09602e5c8dbaa15d617b551e2e0e927aa85
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-31 01:59:59 +0800

    pass test 3 case month 2

--- CarBuyerTest/CarBuyerTest.cs
+++ CarBuyerTest/CarBuyerTest.cs
@@ -24,7 +24,7 @@ namespace CarBuyerTest
         public void Start_price_smaller_then_car_price_should_return_correct_result()
         {
             AllElementsAreEqual(setExceptArray(1,15), PriceCounter.Count(2000, 3000, 1000, 1.5));
-//            AllElementsAreEqual(setExceptArray(2,15), PriceCounter.Count(2000, 4000, 1000, 1.5));
+            AllElementsAreEqual(setExceptArray(2,60), PriceCounter.Count(2000, 4000, 1000, 1.5));
 //            AllElementsAreEqual(setExceptArray(6,766), PriceCounter.Count(2000, 8000, 1000, 1.5));
         }
 
