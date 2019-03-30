aad641018cf774c843ee7a59e88113d82b8e661d
sherlock.chen53 <sherlock.chen53@TI03NB136>
2019-03-31 03:42:35 +0800

    pass test 3

--- CarBuyerTest/PriceCounter.cs
+++ CarBuyerTest/PriceCounter.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarBuyerTest
 {
     class PriceCounter
     {
-        public static int[] Count(int startPrice, int targetStartPrice, int savingPerMonth, double decreasingRate)
+        public static int[] Count(int startPrice, int targetPrice, int savingPerMonth, double startDepreciation)
         {
             
 
             var monthCount = 0;
             double currentStartPrice = startPrice;
-            double currentTargetPrice = targetStartPrice;
+            double currentTargetPrice = targetPrice;
             var currentSaving = 0;
-            double currentDepreciation = decreasingRate/100;
+            var currentDepreciation = startDepreciation/100;
+
+            bool CanBuy()
+            {
+                return currentStartPrice + currentSaving >= currentTargetPrice;
+            }
+
+            bool IsEvenMonth()
+            {
+                return  monthCount % 2 == 1;
+            }
+
+            
             while (true)
             {
-                if (currentStartPrice+currentSaving>=currentTargetPrice)
+                
+                if (CanBuy())
                 {
                     return new[] {monthCount, Convert.ToInt32(currentStartPrice+currentSaving - currentTargetPrice)};
                 }
 
-                if (monthCount!=0&&monthCount % 2 == 0)
+                if (IsEvenMonth())
                 {
                     currentDepreciation += 0.005;
                 }
@@ -32,17 +46,16 @@ namespace CarBuyerTest
                 currentStartPrice *= (1 - currentDepreciation);
 
                 currentTargetPrice *= (1 - currentDepreciation);
-                if (currentStartPrice + currentSaving >= currentTargetPrice)
+//                currentStartPrice -= startPriceOld * currentDepreciation;
+//
+//                currentTargetPrice -=  startPriceNew*currentDepreciation;
+                currentSaving += savingPerMonth;
+                monthCount++;
+
+                if (CanBuy())
                 {
                     return new[] { monthCount, Convert.ToInt32(currentStartPrice + currentSaving - currentTargetPrice) };
                 }
-
-                currentSaving += savingPerMonth;
-                //                currentStartPrice -= startPrice * currentDepreciation;
-
-                //                currentTargetPrice -=  targetStartPrice*currentDepreciation;
-
-                monthCount++;
             }
         }
     }
